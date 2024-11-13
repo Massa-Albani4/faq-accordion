949e9e9d8762080ffd60cf4f160089372a1786f2
Massa-Albani4 <massa.albani0213@gmail.com>
2024-11-13 20:21:55 +0400

    remove plus icon buttons

--- public/main.js
+++ public/main.js
@@ -1,1 +1,8 @@
-const faqRows = document.querySelectorAll(".faq-row");
+const faqRow = document.getElementsByTagName("li");
+
+for (let i = 0; i < faqRow.length; i++) {
+  faqRow[i].addEventListener("click", () => {
+    this.classList.toggle("active");
+    this.style.backgroundColor = "#63535B";
+  });
+}
