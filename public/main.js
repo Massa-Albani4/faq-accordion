339ebc86183fc0f3a1a47d53fea7b2752c4d7fdd
Massa-Albani4 <massa.albani0213@gmail.com>
2024-11-13 23:24:35 +0400

    Add accordion javascript logic

--- public/main.js
+++ public/main.js
@@ -1,8 +1,19 @@
-const faqRow = document.getElementsByTagName("li");
+// Get all the question containers
+const rowContainers = document.querySelectorAll(".row-container");
 
-for (let i = 0; i < faqRow.length; i++) {
-  faqRow[i].addEventListener("click", () => {
-    this.classList.toggle("active");
-    this.style.backgroundColor = "#63535B";
+// Add a click event listener to each question container
+rowContainers.forEach((row) => {
+  const question = row.querySelector("p");
+  const answer = row.querySelector(".answer");
+  const plusIcon = row.querySelector("img");
+
+  question.addEventListener("click", () => {
+    // Toggle the display of the answer
+    answer.classList.toggle("active");
+    if (answer.classList.contains("active")) {
+      plusIcon.src = "../src/assets/icon-minus.svg";
+    } else {
+      plusIcon.src = "../src/assets/icon-plus.svg";
+    }
   });
-}
+});
